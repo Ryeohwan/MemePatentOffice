17f4d322185846e1ae9132eaf12486237678d788
syooj <syooj0728@gmail.com>
2023-03-21 14:35:24 +0900

    [FE]feat: Add nftcard carousel

--- Frontend/src/components/common/NftCard.tsx
+++ Frontend/src/components/common/NftCard.tsx
@@ -1,33 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
+import NftCardFront from "components/common/NftCardFront";
+import NftCardBack from "components/common/NftCardBack";
 import styles from "./NftCard.module.css";
-import totoro from "assets/totoro.jpg";
-import haku from "assets/haku.png";
 
-const NftCard:React.FC = () => {
-    // NFT 제목
-    const NFT_TEXT = "귀여운 토토로 삼형제와 발랄한 자매 사츠키, 메이의 우당탕탕 가족사진입니다아~~~~~"
-    // NFT 제목 글자수 슬라이싱
-    const slicingText = (NFT_TEXT:string) => {
-        if (NFT_TEXT.length > 38) {
-            return NFT_TEXT.substring(0, 38) + " ...";
-        } else {
-            return NFT_TEXT;
-        }
-    }
+interface NftProps {
+    items: {id: number, title: string, imgUrl: string, description: string};
+};
+
+const NftCard:React.FC<NftProps> = (nft) => {
+    const [flipped, setFlipped] = useState<boolean>(false);             // 카드 클릭 시 뒤집기 애니메이션 주기 위한 useState
+    const onClickHandler = (flipped:boolean) => {
+        setFlipped(!flipped);
+      };
 
     return (
-        <div className={styles.nftCardWrapper} >
-            <div className={styles.nftOwner}>
-                <img src={haku} alt="" className={styles.nftOwnerImg}/>
-                <div className={styles.nftOwnerName}>단발머리 부엉이</div>
-            </div>                                                                                  
-            <img src={totoro} alt="" className={styles.nftImg}/>
-            <div className={styles.nftTxtWrapper}>
-                <div className={styles.nftTxt}>
-                    {slicingText(NFT_TEXT)}
-                </div>
+        <div className={`${styles.nftCardContainer} ${flipped ? `${styles.nftCardFlipped}`: null}` } onClick={() => onClickHandler(flipped)}>
+            <div className={styles.nftCardFront}>
+            <NftCardFront items={nft.items} />
             </div>
-        </div>
+            <div className={styles.nftCardBack}>
+            <NftCardBack items={nft.items} />
+            </div>
+      </div>
     );
 };
 
---
+++ Frontend/src/components/common/NftCardFront.tsx
@@ -0,0 +1,39 @@
+import React from "react";
+import styles from "./NftCardFront.module.css";
+import haku from "assets/haku.png";
+
+interface NftProps {
+    items: {id: number, title: string, imgUrl: string, description: string};
+};
+
+const NftCardFront:React.FC<NftProps> = nft => {
+    // NFT 제목
+    const NFT_TEXT = nft.items.title;
+    const NFT_imgUrl = "http://localhost:3000/" + nft.items.imgUrl;
+    
+    // NFT 제목 글자수 슬라이싱
+    const slicingText = (NFT_TEXT:string) => {
+        if (NFT_TEXT.length > 38) {
+            return NFT_TEXT.substring(0, 38) + " ...";
+        } else {
+            return NFT_TEXT;
+        }
+    }
+
+    return (
+        <div className={styles.nftCardWrapper} >
+            <div className={styles.nftOwner}>
+                <img src={haku} alt="" className={styles.nftOwnerImg}/>
+                <div className={styles.nftOwnerName}>단발머리 부엉이</div>
+            </div>                                                                                  
+            <img src={NFT_imgUrl} alt="" className={styles.nftImg}/>
+            <div className={styles.nftTxtWrapper}>
+                <div className={styles.nftTxt}>
+                    {slicingText(NFT_TEXT)}
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default NftCardFront;
