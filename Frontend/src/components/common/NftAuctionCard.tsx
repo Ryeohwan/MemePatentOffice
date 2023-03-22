3330646b2f3067796af6ddd79edf89ef5b845c53
syooj <syooj0728@gmail.com>
2023-03-22 10:34:04 +0900

    [FE]feat: Auction card routing

--- Frontend/src/components/common/NftAuctionCard.tsx
+++ Frontend/src/components/common/NftAuctionCard.tsx
@@ -1,22 +1,38 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
 import styles from "./NftAuctionCard.module.css";
 
-const NftAuctionCard:React.FC = () => {
+interface AuctionProps {
+    items: {meme_id: number, auction_id: number, title: string, time: string, highest_bid: number, imgUrl: string};
+};
+
+const NftAuctionCard:React.FC<AuctionProps> = nft => {
+    const AUCTION_IMG = "http://localhost:3000/" + nft.items.imgUrl;
+    const AUCTION_MEME = nft.items.title;
+    const AUCTION_TIME = nft.items.time;
+    const HIGHEST_BID = nft.items.highest_bid;
+    const MEME_DETAIL_URL = `/meme-detail/:${nft.items.meme_id}`;
+    const AUCTION_DETAIL_URL = `/auction/:${nft.items.auction_id}`;
+
     return (
         <div className={styles.auctionCardWrapper}>
-            <img src="" alt="" className={styles.auctionImg} />
-            <div className={styles.nftTitleText}>!!nft 제목!!</div>
+            <img src={AUCTION_IMG} alt="" className={styles.auctionImg} />
+            <div className={styles.nftTitleText}>{AUCTION_MEME}</div>
             <div>
                 <div>남은 시간</div>
-                <div>!!시간!!</div>
+                <div>{AUCTION_TIME}</div>
             </div>
             <div>
                 <div>최고가</div>
-                <div>!!가격!!</div>
+                <div>{HIGHEST_BID}</div>
             </div>
             <div>
-                <button>상세 보기</button>
-                <button>경매방 입장하기</button>
+                <NavLink to={MEME_DETAIL_URL}>
+                    <button>상세 보기</button>
+                </NavLink>
+                <NavLink to={AUCTION_DETAIL_URL}>
+                    <button>경매방 입장하기</button>
+                </NavLink>
             </div>
 
         </div>
--- Frontend/src/pages/HomePage.tsx
+++ Frontend/src/pages/HomePage.tsx
@@ -2,11 +2,11 @@
 import React from "react";
 import styles from "./HomePage.module.css";
 import { Carousel } from "primereact/carousel";
-import { Button } from "primereact/button";
 import NftCard from "components/common/NftCard";
 import NftAuctionCard from "components/common/NftAuctionCard";
 
 const HomePage: React.FC = () => {
+  // NFT 가짜 데이터
   const NFTS = [
     {
       id: 1,
@@ -31,15 +31,59 @@ const HomePage: React.FC = () => {
         "인기 드라마 더 글로리 속 학교 폭력 가해자 박연진이 같은 무리의 친구(?) 최혜정에게 하는 대사이다. 최혜정이 박연진 남편의 친구 무리에게 박연진의 학창 시절에 대한 이야기를 해서 박연진이 화나서 하는 대사이다.",
     },
   ];
-
-  const carouselTemplate = (nft: {
+  // 경매 가짜 데이터
+  const AUCTIONS = [
+    {
+      meme_id: 1,
+      auction_id: 1,
+      title:
+        "귀여운 토토로 삼형제와 발랄한 자매 사츠키, 메이의 우당탕탕 가족사진입니다",
+      time: "16시간 32분",
+      highest_bid: 430,
+      imgUrl: "totoro.jpg",
+    },
+    {
+      meme_id: 2,
+      auction_id: 3,
+      title: "누가 이렇게 예쁘게 낳았어? 엄마엄마가~",
+      time: "5시간 32분",
+      highest_bid: 200,
+      imgUrl: "newjeans.jpg",
+    },
+    {
+      meme_id: 3,
+      auction_id: 2,
+      title: "알아들었으면 끄덕여",
+      time: "20시간 32분",
+      highest_bid: 500,
+      imgUrl: "theglory.jpeg",
+    },
+  ];
+  // ------------------------------------------------------------------------------
+  // Carousel로 띄울 카드 NFT 밈과 Auction 두 개이므로 타입 두 개 지정
+  type NftObject = {
     id: number;
     title: string;
     imgUrl: string;
     description: string;
-  }) => {
-    // carousel에 넣을 화면 구현
-    return <NftCard items={nft} />;
+  };
+  type AuctionObject = {
+    meme_id: number;
+    auction_id: number;
+    title: string;
+    time: string;
+    highest_bid: number;
+    imgUrl: string;
+  };
+
+  const nftCarousel = (nft: NftObject | AuctionObject) => {
+    // typeof type guard로 체크 불가능 NftObject에 있는 속성이 AuctionObject에는 없어서 비교 자체가 불가
+    // in 키워드로 type guard하면 됨
+    if ("description" in nft) {
+      return <NftCard items={nft} />;
+    } else {
+      return <NftAuctionCard items={nft}/>
+    }
   };
 
   return (
@@ -49,13 +93,21 @@ const HomePage: React.FC = () => {
         value={NFTS}
         numVisible={1}
         numScroll={1}
-        itemTemplate={(page) => carouselTemplate(page)}
+        itemTemplate={(page) => nftCarousel(page)}
         orientation={"horizontal"}
         showIndicators={false}
         circular={true}
-
       />
-      <NftAuctionCard/>
+      <Carousel
+        page={1}
+        value={AUCTIONS}
+        numVisible={1}
+        numScroll={1}
+        itemTemplate={(page) => nftCarousel(page)}
+        orientation={"horizontal"}
+        showIndicators={false}
+        circular={true}
+      />
     </div>
   );
 };
