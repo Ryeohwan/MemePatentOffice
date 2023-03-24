ba322bfdc8632aedce8904e4b36778ac3187ad51
developerhongjulee <developerhongjulee@gmail.com>
2023-03-24 18:02:43 +0900

    [BE]feat: add swagger config(build error)

--- auction/src/main/java/com/memepatentoffice/auction/config/MongoConfig.java
+++
@@ -1,8 +0,0 @@
-package com.memepatentoffice.auction.config;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.data.mongodb.config.EnableMongoAuditing;
-
-@Configuration
-public class MongoConfig {
-}
