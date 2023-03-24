bfc4c34099db839fd07139f319b75940fa9ef7b3
안려환 <arh951019@gmail.com>
2023-03-24 17:55:11 +0900

    :art: [BE]안려환:update feature
    update features

--- mpoffice/src/main/java/com/memepatentoffice/mpoffice/db/entity/Notification.java
+++ mpoffice/src/main/java/com/memepatentoffice/mpoffice/db/entity/Notification.java
@@ -3,16 +3,14 @@ package com.memepatentoffice.mpoffice.db.entity;
 import lombok.Getter;
 import lombok.Setter;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.Lob;
+import javax.persistence.*;
 import java.time.LocalDateTime;
 
 @Getter
 @Entity
 public class Notification {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "notification_id", nullable = false)
     private Long id;
 
