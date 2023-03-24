6123bd24b13adceec8cb056a8b43c2598a727064
developerhongjulee <developerhongjulee@gmail.com>
2023-03-24 16:23:50 +0900

    [BE]feat: add AuctionCreationReq, add enrollAuction, getAuctionAll, getAuctionById to AuctionController

--- auction/src/main/java/com/memepatentoffice/auction/api/controller/AuctionController.java
+++ auction/src/main/java/com/memepatentoffice/auction/api/controller/AuctionController.java
@@ -1,11 +1,49 @@
 package com.memepatentoffice.auction.api.controller;
 
+import com.memepatentoffice.auction.api.request.AuctionCreationReq;
 import com.memepatentoffice.auction.api.service.AuctionService;
+import com.memepatentoffice.auction.common.exception.NotFoundException;
+import com.memepatentoffice.auction.util.StartAuction;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
-@RestController
+import java.sql.Timestamp;
+import java.time.LocalDateTime;
+import java.util.Date;
+import java.util.Timer;
+
+@RestController("/api/auction")
 @RequiredArgsConstructor
 public class AuctionController {
-    private AuctionService auctionService;
+    private final AuctionService auctionService;
+
+    @PostMapping("/")
+    public ResponseEntity<?> enrollAuction(@RequestBody AuctionCreationReq auctionCreationReq){
+        LocalDateTime startLocalDateTime = auctionCreationReq.getStartDateTime();
+        Date startDate = Timestamp.valueOf(startLocalDateTime);
+        new Timer().schedule(
+                StartAuction.builder()
+                        .auctionService(auctionService)
+                        .memeId(auctionCreationReq.getMemeId())
+                        .build(),
+                startDate);
+        //기다리는지 비동기인지?
+        return ResponseEntity.status(HttpStatus.CREATED).body(null);
+    }
+    @GetMapping("/")
+    public ResponseEntity<?> getAuctionAll(){
+        return ResponseEntity.status(HttpStatus.OK).body(
+                auctionService.findAll()
+        );
+    }
+
+    @GetMapping("/{auctionId}")
+    public ResponseEntity<?> getAuctionById(@PathVariable Long auctionId) throws NotFoundException {
+        return ResponseEntity.status(HttpStatus.OK).body(
+          auctionService.findById(auctionId)
+        );
+    }
+
 }
--- auction/src/main/java/com/memepatentoffice/auction/api/request/AuctionCreationReq.java
+++ auction/src/main/java/com/memepatentoffice/auction/api/request/AuctionCreationReq.java
@@ -1,4 +1,16 @@
 package com.memepatentoffice.auction.api.request;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
 public class AuctionCreationReq {
+    private Long memeId;
+    private LocalDateTime startDateTime;
+
 }
