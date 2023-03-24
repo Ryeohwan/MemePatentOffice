438cb5314fbb7229732dc3f597fed7d852929893
developerhongjulee <developerhongjulee@gmail.com>
2023-03-24 13:09:39 +0900

    [BE]feat: Mongodb config

--- auction/src/main/java/com/memepatentoffice/auction/config/MongoConfig.java
+++ auction/src/main/java/com/memepatentoffice/auction/config/MongoConfig.java
@@ -1,7 +1,9 @@
 package com.memepatentoffice.auction.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.mongodb.config.EnableMongoAuditing;
 
 @Configuration
+@EnableMongoAuditing
 public class MongoConfig {
 }
