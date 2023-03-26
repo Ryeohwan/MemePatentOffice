03c16b59a3008af5c30cd47bc19da5aaaf910ac3
조현철 <chaoman5@naver.com>
2023-03-26 14:18:50 +0900

    [FE]feat: Add myCommentPage in settings

--- Frontend/src/pages/setting/CommentHistoryPage.tsx
+++ Frontend/src/pages/setting/CommentHistoryPage.tsx
@@ -1,11 +1,59 @@
 // 내가 쓴 댓글 page (/setting/history/comment)
+import React from "react";
+
+import { useSelector, useDispatch } from "react-redux";
+import { RootState } from "store/configStore";
+import { historyActions } from "store/history";
+import { myCommentList } from "store/history";
+
+import styles from "pages/setting/CommentHistoryPage.module.css";
+import "pages/setting/Setting.css";
+import { Divider } from "primereact/divider";
+import CommentHistoryItem from "components/settings/history/CommentHistoryItem";
 
 const CommentHistoryPage: React.FC = () => {
+  const today = useSelector<RootState, myCommentList[]>(
+    (state) => state.history.today
+  );
+  const week = useSelector<RootState, myCommentList[]>(
+    (state) => state.history.week
+  );
+  const month = useSelector<RootState, myCommentList[]>(
+    (state) => state.history.month
+  );
+
   return (
-   <>
-     <h1>CommentHistoryPage</h1>
-   </>
-  )
- }
- 
- export default CommentHistoryPage; 
+    <div className="wrapper">
+      <p className="pageName">내가 쓴 댓글</p>
+      <Divider className="divider" />
+      <>
+        <p className={styles.date}>오늘</p>
+        {today.length > 0 &&
+          today.map((item) => {
+            return <CommentHistoryItem item={item} />;
+          })}
+      </>
+      {week.length > 0 && (
+        <>
+          <Divider />
+          <p className={styles.date}>이번 주</p>
+          {week.map((item) => {
+            return <CommentHistoryItem item={item} />;
+          })}
+        </>
+      )}
+
+      {month.length > 0 && (
+        <>
+          <Divider />
+          <p className={styles.date}>이번 달</p>
+          {month.map((item) => {
+            return <CommentHistoryItem item={item} />;
+          })}
+        </>
+      )}
+    </div>
+  );
+};
+
+export default CommentHistoryPage;
--- Frontend/src/pages/setting/PurchaseSaleHistoryPage.tsx
+++ Frontend/src/pages/setting/PurchaseSaleHistoryPage.tsx
@@ -1,14 +1,18 @@
 // 구매한 nft page (/setting/auction-history/purchase)
 import React, { useRef } from "react";
 import { useLocation } from "react-router-dom";
+
 import { useSelector, useDispatch } from "react-redux";
 import { RootState } from "store/configStore";
 import { nftHistoryActions } from "store/nftHistory";
 import { myHistoryList } from "store/nftHistory"; // type
 
-import styles from "pages/setting/AuctionHistory.module.css";
+import PurchaseSaleNftItem from "components/settings/auction/PurchaseSaleNftItem";
 import { Divider } from "primereact/divider";
-import PurchaseSaleNftItem from "components/settings/history/PurchaseSaleNftItem";
+
+import styles from "pages/setting/AuctionHistory.module.css";
+import 'pages/setting/Setting.css'
+
 
 const PurchaseHistoryPage: React.FC = () => {
   const location = useLocation(); // sale, purchase인지 구분하기 위해 사용
@@ -17,14 +21,14 @@ const PurchaseHistoryPage: React.FC = () => {
   ); // 내 history 리스트 Redux에서 가져오기
   const dateList = useRef<string[]>([]);
   return (
-    <div className={styles.wrapper}>
+    <div className='wrapper'>
       {/* 구매한, 판매한 구분 */}
       {location.pathname.includes("purchase") ? (
-        <p className={styles.pageName}>내가 구매한 NFT</p>
+        <p className='pageName'>내가 구매한 NFT</p>
       ) : (
-        <p className={styles.pageName}>내가 판매한 NFT</p>
+        <p className='pageName'>내가 판매한 NFT</p>
       )}
-      <Divider className={styles.divider} />
+      <Divider className='divider' />
       {myHistoryList.map((item, index) => {
         const formatDate = item.date.split("T")[0];
         if (dateList.current.includes(formatDate)) {
