9d04ea5d5b91fdca417c88cac6b78b3da4c9f42b
developerhongjulee <developerhongjulee@gmail.com>
2023-03-27 10:59:25 +0900

    [BE]feat: add swagger, websocket, dockerfile, docker-compose, SecurityConfig, webMvcConfig

--- auction/src/main/java/com/memepatentoffice/auction/AuctionApplication.java
+++ auction/src/main/java/com/memepatentoffice/auction/AuctionApplication.java
@@ -2,12 +2,8 @@ package com.memepatentoffice.auction;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
-import org.springframework.data.mongodb.config.EnableMongoAuditing;
 
 @SpringBootApplication
-@EnableJpaAuditing
-@EnableMongoAuditing
 public class AuctionApplication {
 
 	public static void main(String[] args) {
