319ff510690b4b84974af8322db208d344e0de1c
haun <ryuhaun0321@gmail.com>
2023-03-27 22:04:45 +0900

    [FE]feat: Add meme-upload Content Moderator 429 error handling test

--- Frontend/src/components/meme/upload/MemeUploadBtn.tsx
+++ Frontend/src/components/meme/upload/MemeUploadBtn.tsx
@@ -1,4 +1,4 @@
-import axios from "axios";
+import axios, { AxiosError } from "axios";
 import React, { useRef, useState, useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import memeUpload, { memeUploadActions } from "store/memeUpload";
@@ -13,13 +13,9 @@ const MemeUploadBtn: React.FC = () => {
   const dispatch = useDispatch();
   const title = useSelector<RootState, string>((state) => state.memeUpload.title);
   const titleChecked = useSelector<RootState, boolean>((state) => state.memeUpload.titleChecked);
-  const titleState = useSelector<RootState, number>((state) => state.memeUpload.titleState);
   const imgUrl = useSelector<RootState, string>((state) => state.memeUpload.imgUrl);
-  const imgState = useSelector<RootState, number>((state) => state.memeUpload.imgState);
   const info = useSelector<RootState, string>((state) => state.memeUpload.info);
-  const infoState = useSelector<RootState, number>((state) => state.memeUpload.infoState);
   const situation = useSelector<RootState, string>((state) => state.memeUpload.situation);
-  const situationState = useSelector<RootState, number>((state) => state.memeUpload.situationState);
   const checkbox = useSelector<RootState, boolean>((state) => state.memeUpload.checkbox);
 
   // 유효성 검사 모달 관련
@@ -60,7 +56,12 @@ const MemeUploadBtn: React.FC = () => {
         return 1; // 유해성 통과 o
       }
     } catch (error) {
-      console.log(error);
+      const { response } = error as unknown as AxiosError;
+      console.log('error here', response)
+      // 접속자수 많은 axios error난 경우
+      if (response!.status === 429) {
+        return 2  
+      }
       return 0;
     }
   };
@@ -105,6 +106,7 @@ const MemeUploadBtn: React.FC = () => {
     }
   };
 
+
   // click하면 input 확인 -> 유해성검사 -> nft 등록 / 백 api
   const uploadHandler = async () => {
     // title 중복검사 통과 + 나머지 모든 값 존재 시에만 upload
@@ -114,11 +116,19 @@ const MemeUploadBtn: React.FC = () => {
     }
 
     // 유해성검사
-    controlCheckModal(true);
+    controlCheckModal(true);  // modal 띄우기
+    
     setModalTxt("제목 유해성 검사중...");
     const titleMonitor = await textMonitor(title);
     await new Promise((resolve) => setTimeout(resolve, 1000));
-
+    
+    // 동시에 시도해서 axios error난 경우
+    if (titleMonitor === 2) {
+      controlCheckModal(false);
+      alert('잠시후 다시 시도해주세요..');
+      return;
+    }
+    
     setModalTxt("이미지 유해성 검사중...");
     const imgMonitor = await imageMonitor(imgUrl);
     await new Promise((resolve) => setTimeout(resolve, 2000));
@@ -137,12 +147,7 @@ const MemeUploadBtn: React.FC = () => {
     dispatch(memeUploadActions.setSituationState(situationMonitor));
 
     // 유해성 검사 모두 통과한 경우
-    if (
-      titleMonitor > 0 ||
-      imgMonitor > 0 ||
-      infoMonitor > 0 ||
-      situationMonitor > 0
-    ) {
+    if (titleMonitor > 0 || imgMonitor > 0 || infoMonitor > 0 || situationMonitor > 0) {
       // nft 등록
       setModalTxt("NFT 등록중...");
       await new Promise((resolve) => setTimeout(resolve, 3000));
@@ -155,6 +160,7 @@ const MemeUploadBtn: React.FC = () => {
       // 임시 ALERT
       alert("임시 nft 등록 성공!");
     } else {
+      // 하나라도 유해성검사 통과 못한 경우
       controlCheckModal(false);
     }
   };
