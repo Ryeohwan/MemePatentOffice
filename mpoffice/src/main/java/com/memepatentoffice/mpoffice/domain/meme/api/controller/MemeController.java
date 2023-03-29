1e296cb2a71a7d45352d277cea9ac63d1f46b79c
RyeoHwan <arh951019@gmail.com>
2023-03-29 11:14:46 +0900

    [BE] 안려환: request mapping update

--- mpoffice/src/main/java/com/memepatentoffice/mpoffice/domain/meme/api/controller/MemeController.java
+++ mpoffice/src/main/java/com/memepatentoffice/mpoffice/domain/meme/api/controller/MemeController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.*;
 
 
 @RequiredArgsConstructor
-@RequestMapping("api/mpoffice/meme")
+@RequestMapping("/api/mpoffice/meme")
 @RestController
 public class MemeController {
     private final MemeService memeService;
