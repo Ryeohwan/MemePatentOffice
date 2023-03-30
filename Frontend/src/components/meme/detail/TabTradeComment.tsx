858ee1c192b64c8922a9ec47e625350737438b70
DESKTOP-DOGVPUB\SSAFY <ryuhaun0321@gmail.com>
2023-03-30 18:09:54 +0900

    [FE]feat: Add meme-detail tab params

--- Frontend/src/components/meme/detail/TabTradeComment.tsx
+++ Frontend/src/components/meme/detail/TabTradeComment.tsx
@@ -1,14 +1,14 @@
 import React from "react";
+import { Route, useParams } from "react-router-dom";
 import TabComp from "components/common/tab/TabComp";
-import { Route } from "react-router-dom";
-import styles from "./TabTradeComment.module.css";
 import ChartTradeComp from "./ChartTradeComp";
 import CommentTab from "../comment/CommentTab";
+import styles from "./TabTradeComment.module.css";
 
 
 const TabTradeComment:React.FC = () => {
-
-    const MEME_ID = 1;
+  const params = useParams();
+  const MEME_ID = params.meme_id;
 
     const tabItems = [
         { name: "거래", path: `/meme-detail/${MEME_ID}/tab=trade` },    
