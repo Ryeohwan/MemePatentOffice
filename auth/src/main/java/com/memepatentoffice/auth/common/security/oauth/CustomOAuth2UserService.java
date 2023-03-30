43be563c7d40fce46e8cce77a96314f833d82900
이예은 <dodamond222@gmail.com>
2023-03-30 20:36:16 +0900

    [BE] feat: change nickname setting2

--- auth/src/main/java/com/memepatentoffice/auth/common/security/oauth/CustomOAuth2UserService.java
+++ auth/src/main/java/com/memepatentoffice/auth/common/security/oauth/CustomOAuth2UserService.java
@@ -83,7 +83,7 @@ public class CustomOAuth2UserService extends DefaultOAuth2UserService {
 
 
         return userService.registUser(UserRequest.builder()
-                .nickname(oAuth2UserInfo.getEmail().split("@")[0])
+                .nickname("MEME_" + oAuth2UserInfo.getEmail().split("@")[0])
                 .email(oAuth2UserInfo.getEmail())
                 .build());
     }
