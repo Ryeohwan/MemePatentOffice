4f5bdbb22b11cd99fb96d52d60eb7c5270294374
조현철 <chaoman5@naver.com>
2023-04-02 16:08:38 +0900

    [FE]feat: Add resizeHandler (maxWidth:420)

---
+++ Frontend/src/pages/ResizePage.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import styles from 'pages/ResizePage.module.css'
+
+const ResizePage:React.FC = () => {
+    return (
+    <div className={styles.container}>
+        <p className={styles.text}>서유진 여기 채워줘</p>
+    </div>
+    )
+}
+
+export default ResizePage
