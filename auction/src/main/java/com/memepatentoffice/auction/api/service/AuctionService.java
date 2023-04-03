0ff2eaceb44d823aae2dd09e03ff490f8ae75167
developerhongjulee <developerhongjulee@gmail.com>
2023-04-03 10:55:29 +0900

    [BE]feat: add findAllByHit, findAllByStartDate to AuctionService

--- auction/src/main/java/com/memepatentoffice/auction/api/service/AuctionService.java
+++ auction/src/main/java/com/memepatentoffice/auction/api/service/AuctionService.java
@@ -3,14 +3,18 @@ package com.memepatentoffice.auction.api.service;
 import com.memepatentoffice.auction.api.message.WebSocketCharacter;
 import com.memepatentoffice.auction.api.request.AuctionCreationReq;
 import com.memepatentoffice.auction.api.message.WebSocketChatReq;
+import com.memepatentoffice.auction.api.response.AuctionRes;
 import com.memepatentoffice.auction.common.exception.NotFoundException;
 
 import java.io.IOException;
+import java.util.List;
 
 public interface AuctionService {
 
     Long enrollAuction(AuctionCreationReq auctionCreationReq) throws NotFoundException, IOException;
     void sendChat(WebSocketChatReq req);
+    List<AuctionRes> findAllByHit();
+    List<AuctionRes> findAllByStartDate();
 
     void sendCharacter(WebSocketCharacter dto);
 }
