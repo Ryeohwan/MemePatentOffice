ea38c1c6bd979db52acb578804c30c6b896f4cfe
developerhongjulee <developerhongjulee@gmail.com>
2023-04-03 18:17:08 +0900

    [BE]fix: remove redundant codes

--- auction/src/main/java/com/memepatentoffice/auction/db/entity/Auction.java
+++ auction/src/main/java/com/memepatentoffice/auction/db/entity/Auction.java
@@ -40,27 +40,5 @@ public class Auction extends BaseEntity{
     public LocalDateTime getFinishTime(){
         return this.startTime.plusMinutes(Auction.AUCTION_DURATION_MINUTES);
     }
-    public boolean start(){
-        if(AuctionStatus.ENROLLED.equals(this.status)){ //Enum에 equals 사용 시 내부적으로 ==비교를 하긴 함
-            this.status = AuctionStatus.PROCEEDING;
-            return true;
-        }
-        else{
-            log.warn("경매가 대기중이 아닌데 진행중으로 변경하려 합니다");
-            log.warn("현재 상태: "+this.status.toString());
-            return false;
-        }
-    }
-    public boolean terminate(){
-        if(AuctionStatus.PROCEEDING.equals(this.status)){ //Enum에 equals 사용 시 내부적으로 ==비교를 하긴 함
-            this.status = AuctionStatus.TERMINATED;
-            return true;
-        }
-        else{
-            log.warn("경매가 진행중이 아닌데 끝내려고 합니다");
-            log.warn("현재 상태"+this.status.toString());
-            return false;
-        }
-    }
 }
 
