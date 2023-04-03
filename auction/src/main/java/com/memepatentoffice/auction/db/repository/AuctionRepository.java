0490e53ecf837590211c9ca898d37f5baf1ec8f2
developerhongjulee <developerhongjulee@gmail.com>
2023-04-03 10:56:54 +0900

    [BE]feat: add findAllProceeding, findAllProceedingByStartDate to AuctionRepository

--- auction/src/main/java/com/memepatentoffice/auction/db/repository/AuctionRepository.java
+++ auction/src/main/java/com/memepatentoffice/auction/db/repository/AuctionRepository.java
@@ -2,6 +2,15 @@ package com.memepatentoffice.auction.db.repository;
 
 import com.memepatentoffice.auction.db.entity.Auction;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface AuctionRepository extends JpaRepository<Auction, Long> {
+    @Query("select a from Auction a where where a.status = com.memepatentoffice.auction.db.entity.type.AuctionStatus.PROCEEDING")
+    List<Auction> findAllProceeding();
+
+    @Query("select a from Auction a where a.status = com.memepatentoffice.auction.db.entity.type.AuctionStatus.PROCEEDING order by a.startTime")
+    List<Auction> findAllProceedingByStartDate();
 }
