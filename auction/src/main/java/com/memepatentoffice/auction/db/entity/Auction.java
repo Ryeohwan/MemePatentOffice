3280492e5a921535f198c68607e0a496bf398161
developerhongjulee <developerhongjulee@gmail.com>
2023-04-04 13:11:31 +0900

    [BE]feat: denormalization - add nickname to Auction, Bid entity

--- auction/src/main/java/com/memepatentoffice/auction/db/entity/Auction.java
+++ auction/src/main/java/com/memepatentoffice/auction/db/entity/Auction.java
@@ -1,6 +1,5 @@
 package com.memepatentoffice.auction.db.entity;
 
-import com.fasterxml.jackson.annotation.JsonFormat;
 import com.memepatentoffice.auction.db.entity.type.AuctionStatus;
 import lombok.*;
 import lombok.extern.slf4j.Slf4j;
@@ -8,12 +7,10 @@ import org.springframework.format.annotation.DateTimeFormat;
 
 import javax.persistence.*;
 import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
 
 @Entity
 @Getter
 @NoArgsConstructor
-@Builder
 @Slf4j
 @ToString
 @Table(name = "auction")
@@ -27,17 +24,27 @@ public class Auction extends BaseEntity{
     private LocalDateTime startTime;
     @Column(name = "seller_id")
     private Long sellerId;
+
+    @Column(name = "seller_nickname")
+    private String sellerNickname;
+
+    @Column(name = "starting_price")
+    private Long startingPrice;
+
     @Enumerated(EnumType.STRING)
     @Column(name = "status")
     private AuctionStatus status;
 
     private static final Integer AUCTION_DURATION_MINUTES = 60*24;
     @Builder
-    public Auction(Long memeId, LocalDateTime startTime, Long sellerId, AuctionStatus status) {
+    public Auction(Long memeId, LocalDateTime startTime, Long sellerId,
+                   String sellerNickname, Long startingPrice) {
         this.memeId = memeId;
         this.startTime = startTime;
         this.sellerId = sellerId;
-        this.status = status;
+        this.sellerNickname = sellerNickname;
+        this.startingPrice = startingPrice;
+        this.status = AuctionStatus.ENROLLED;
     }
     public LocalDateTime getFinishTime(){
         return this.startTime.plusMinutes(Auction.AUCTION_DURATION_MINUTES);
