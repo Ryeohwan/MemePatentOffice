21d3543ce1eb4653fe3f9c4c3b371944d8ac786f
RyeoHwan <arh951019@gmail.com>
2023-04-04 17:32:50 +0900

    [BE] feat: handling null add

--- mpoffice/src/main/java/com/memepatentoffice/mpoffice/domain/meme/db/repository/SearchRepository.java
+++ mpoffice/src/main/java/com/memepatentoffice/mpoffice/domain/meme/db/repository/SearchRepository.java
@@ -8,6 +8,7 @@ import org.springframework.data.repository.query.Param;
 
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Objects;
 
 public interface SearchRepository extends JpaRepository<Search , Long> {
     Search findSearchByTitle(String title);
@@ -15,12 +16,19 @@ public interface SearchRepository extends JpaRepository<Search , Long> {
     Boolean existsByTitle(String title);
     int countAllByCreatedAtAndTitle(LocalDateTime createdAt, String title);
     int countAllByTitleAndCreatedAtBetween(String title, LocalDateTime from , LocalDateTime to);
+    // 스케줄러 써서 10분에 한 번 하거나 !!!!
+    @Query("SELECT s.title, count(*) as sc " +
+            "FROM Search s " +
+            "WHERE s.date BETWEEN :oneWeekAgo AND :time " + // 검색 기간 조건 추가
+            "GROUP BY s.title " +
+            "ORDER BY sc DESC ")
+    List<Objects> findDailyRanking(@Param("time") LocalDateTime time, @Param("oneWeekAgo") LocalDateTime oneDayAgo);
 
-    List<Search> findAll();
+    @Query("SELECT s.title, count(*) as sc " +
+            "FROM Search s " +
+            "WHERE s.date BETWEEN :oneWeekAgo AND :time " + // 검색 기간 조건 추가
+            "GROUP BY s.title " +
+            "ORDER BY sc DESC ")
+    List<Search> findWeeklyRanking(@Param("time") LocalDateTime time, @Param("oneWeekAgo") LocalDateTime oneWeekAgo);
 
-    @Query("SELECT s.title , count(c) as sc " +
-            "from Search s " +
-            " left join Search c on s.title = c.title " +
-            " order by sc desc ")
-    List<Search> findDailyRanking(@Param("time")LocalDateTime time);
 }
