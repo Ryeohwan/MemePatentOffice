a9abf1e478dec0c06c8f05e00a2a0d6e97aa882c
Hongju Lee <developerhongjulee@gmail.com>
2023-04-04 21:24:21 +0900

    [BE]feat: getListByUserNickName 추가

--- auction/src/main/java/com/memepatentoffice/auction/db/repository/AuctionRepository.java
+++ auction/src/main/java/com/memepatentoffice/auction/db/repository/AuctionRepository.java
@@ -13,6 +13,8 @@ public interface AuctionRepository extends JpaRepository<Auction, Long> {
     @Query("select a from Auction a where a.status = com.memepatentoffice.auction.db.entity.type.AuctionStatus.PROCEEDING")
     List<Auction> findAllProceeding();
 
+    List<Auction> findAllBySellerNickname(@Param("sellerNickname")String sellerNickname);
+
     @Query("select a from Auction a where a.status = com.memepatentoffice.auction.db.entity.type.AuctionStatus.PROCEEDING order by a.startTime")
     List<Auction> findAllProceedingByFinishTimeOldest();
 
