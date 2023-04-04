439ed9414fcb41784d290978ca6dcb65bdea8c69
developerhongjulee <developerhongjulee@gmail.com>
2023-04-04 13:35:24 +0900

    [BE]feat: 경매 진행 중인지 검사하는 로직 추가

--- auction/src/main/java/com/memepatentoffice/auction/api/service/BidServiceImpl.java
+++ auction/src/main/java/com/memepatentoffice/auction/api/service/BidServiceImpl.java
@@ -4,7 +4,9 @@ import com.memepatentoffice.auction.api.dto.request.BidReq;
 import com.memepatentoffice.auction.common.exception.BiddingException;
 import com.memepatentoffice.auction.common.exception.NotFoundException;
 import com.memepatentoffice.auction.common.util.InterServiceCommunicationProvider;
+import com.memepatentoffice.auction.db.entity.Auction;
 import com.memepatentoffice.auction.db.entity.Bid;
+import com.memepatentoffice.auction.db.entity.type.AuctionStatus;
 import com.memepatentoffice.auction.db.repository.AuctionRepository;
 import com.memepatentoffice.auction.db.repository.BidRepository;
 import lombok.RequiredArgsConstructor;
@@ -22,12 +24,15 @@ public class BidServiceImpl implements BidService {
     private final InterServiceCommunicationProvider isp;
     @Override
     public Long bid(BidReq bidReq) throws NotFoundException, BiddingException {
-        if(!auctionRepository.existsById(bidReq.getAuctionId())) throw new NotFoundException("유효하지 않은 auction ID입니다");
+        Auction auction = auctionRepository.findById(bidReq.getAuctionId())
+                .orElseThrow(()->new NotFoundException("auctionId가 유효하지 않습니다"));
+        if(!AuctionStatus.PROCEEDING.equals(auction.getStatus())){
+            throw new BiddingException("아직 시작되지 않았거나 종료된 경매입니다");
+        }
         String respBody = isp.findUserById(bidReq.getUserId())
                 .orElseThrow(()->new NotFoundException("sellerId가 유효하지 않습니다"));
         JSONObject jsonObject = new JSONObject(respBody);
         String nickname = jsonObject.getString("nickname");
-
 
         Bid currentTopBid = bidRepository.findTopByOrderByCreatedAtDesc();
         if(!(bidReq.getAskingprice() > currentTopBid.getAskingprice())){
