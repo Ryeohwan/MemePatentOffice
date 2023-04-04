80f780ddc8f2b5bc199bb8d3f03aa8d315f44294
DESKTOP-DOGVPUB\SSAFY <ryuhaun0321@gmail.com>
2023-04-04 11:21:35 +0900

    [FE]feat: Add scroll top btn in auction-list

--- Frontend/src/components/auction/list/AuctionListPopular.tsx
+++ Frontend/src/components/auction/list/AuctionListPopular.tsx
@@ -1,24 +1,23 @@
 import { useEffect } from "react";
 import useAxios from "hooks/useAxios";
 import { auctionCardType } from "store/auction";
-import NftAuctionCard from "components/common/card/NftAuctionCard";
-import styles from "./AuctionListTabComp.module.css";
 import SkeletonCard from "components/common/card/SkeletonCard";
+import NftAuctionCard from "components/common/card/NftAuctionCard";
+import { ScrollTop } from "primereact/scrolltop";
+import styles from "./AuctionListTabComp.module.css";
 
 const AuctionListPopular: React.FC = () => {
   const { data, status, isLoading, sendRequest } = useAxios();
 
   useEffect(() => {
-    console.log('popular get!')
+    console.log("popular get!");
     sendRequest({ url: `/api/auction/list?sort=popular` });
   }, []);
 
   return (
     <div className={styles.auctionListCardContainer}>
       {/* loading 중 */}
-      {isLoading && (
-        <SkeletonCard />
-      )}
+      {isLoading && <SkeletonCard />}
 
       {/* data 없는 경우 */}
       {status === 200 && data.length === 0 && (
@@ -28,11 +27,26 @@ const AuctionListPopular: React.FC = () => {
       )}
 
       {/* data 들어온 경우 */}
-      {status === 200 && data.length > 0 && (
+      {status === 200 &&
+        data.length > 0 &&
         data.map((auction: auctionCardType) => {
           return <NftAuctionCard key={auction.auctionId} items={auction} />;
-        })
-      )}
+        })}
+
+      <ScrollTop
+        target="parent"
+        threshold={100}
+        icon="pi pi-arrow-up text-base"
+        style={{
+          position: "fixed",
+          marginLeft: "0",
+          bottom: "10%",
+          right: "16px",
+          background: "var(--button-color)",
+          width: "44px",
+          height: "44px",
+        }}
+      />
     </div>
   );
 };
