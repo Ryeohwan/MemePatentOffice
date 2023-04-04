b30a5b099182c60ef36f3c3bdf0cfb7e5e8fb19d
developerhongjulee <developerhongjulee@gmail.com>
2023-04-04 15:27:43 +0900

    [BE]feat: rename function, create function postRequestToUrl, addTransaction

--- auction/src/main/java/com/memepatentoffice/auction/common/util/InterServiceCommunicationProvider.java
+++ auction/src/main/java/com/memepatentoffice/auction/common/util/InterServiceCommunicationProvider.java
@@ -1,10 +1,10 @@
 package com.memepatentoffice.auction.common.util;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.memepatentoffice.auction.api.dto.AuctionClosing;
 import lombok.extern.slf4j.Slf4j;
-import okhttp3.MediaType;
-import okhttp3.OkHttpClient;
-import okhttp3.Request;
-import okhttp3.Response;
+import okhttp3.*;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
@@ -16,12 +16,13 @@ public class InterServiceCommunicationProvider {
     //TODO: baseUrl을 생성자 파라미터로 받기 위해 팩토리로 만들기
     private final String MPOFFICE_SERVER_URL = "https://j8a305.p.ssafy.io/api/mpoffice";
     private final OkHttpClient client = new OkHttpClient();
+    private final ObjectMapper mapper = new ObjectMapper();
     private final MediaType JSON = MediaType.get("application/json; charset=utf-8");
     /*
     *    @Return
     * 404 not found인 경우 null을 반환한다
     * */
-    public Optional<String> getResponsefromOtherServer(String url){
+    public Optional<String> getRequestToUrl(String url){
         Request request = new Request.Builder()
                 .url(url)
                 .build();
@@ -39,11 +40,40 @@ public class InterServiceCommunicationProvider {
             throw new RuntimeException(ex);
         }
     }
+
+    public Optional<String> postRequestToUrl(String url, Object o) throws JsonProcessingException {
+        RequestBody body = RequestBody.create(
+                mapper.writeValueAsString(o),
+                JSON
+        );
+        Request request = new Request.Builder()
+                .url(url)
+                .post(body)
+                .build();
+        try(Response response = client.newCall(request).execute()){
+            if(response.isSuccessful()){
+                String respBody = response.body().string();
+                log.info(respBody);
+                return Optional.of(respBody);
+            }
+            else{
+                log.error("InterServiceCommunicationProvider returned "+response.code()+" by "+url);
+                return Optional.empty();
+            }
+        }catch (IOException ex){
+            throw new RuntimeException(ex);
+        }
+    }
+
+    public Optional<String> addTransaction(AuctionClosing a) throws JsonProcessingException{
+        return postRequestToUrl(MPOFFICE_SERVER_URL+"/meme/addTransaction", a);
+    }
+
     public Optional<String> findUserById(Long userId){
-        return getResponsefromOtherServer(MPOFFICE_SERVER_URL+"/user/"+userId);
+        return getRequestToUrl(MPOFFICE_SERVER_URL+"/user/"+userId);
     }
     public Optional<String> findMemeById(Long memeId){
-        return getResponsefromOtherServer(MPOFFICE_SERVER_URL+"/meme/"+memeId);
+        return getRequestToUrl(MPOFFICE_SERVER_URL+"/meme/"+memeId);
     }
     public boolean existsUserById(Long userId){
         return findUserById(userId).isPresent();
