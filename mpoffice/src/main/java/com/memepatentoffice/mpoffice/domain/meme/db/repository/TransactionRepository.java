c775180059469741db1eef0a87644e38a8bb45aa
RyeoHwan <arh951019@gmail.com>
2023-04-05 17:03:36 +0900

    [BE] feat: priceGraph update

--- mpoffice/src/main/java/com/memepatentoffice/mpoffice/domain/meme/db/repository/TransactionRepository.java
+++ mpoffice/src/main/java/com/memepatentoffice/mpoffice/domain/meme/db/repository/TransactionRepository.java
@@ -3,6 +3,7 @@ package com.memepatentoffice.mpoffice.domain.meme.db.repository;
 import com.memepatentoffice.mpoffice.db.entity.Transaction;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
@@ -10,11 +11,11 @@ public interface TransactionRepository extends JpaRepository<Transaction, Long> 
     @Query("SELECT t.price from Transaction t order by t.createdAt desc ")
     List<Double> findPriceList(Long memeId);
 
-    @Query("SELECT t.price from Transaction t order by t.price desc ")
-    List<Double> PricaRankList(Long memeId);
+    @Query("SELECT t.price from Transaction t where t.memeId = :memeId order by t.price desc ")
+    List<Double> PricaRankList(@Param("memeId") Long memeId);
 
-    @Query("SELECT t from Transaction t order by t.createdAt desc ")
-    List<Transaction> findBuyerList();
+    @Query("SELECT t from Transaction t where t.memeId = :memeId order by t.createdAt desc ")
+    List<Transaction> findBuyerList(@Param("memeId") Long memeId);
 
     List<Transaction> findAllByBuyerId(Long userId);
     List<Transaction> findAllBySellerId(Long userId);
