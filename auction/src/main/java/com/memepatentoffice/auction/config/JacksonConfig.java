b78754ed09efd51a99551b9f38b8c6aca8cff391
developerhongjulee <developerhongjulee@gmail.com>
2023-04-06 15:46:41 +0900

    [BE]fix: fix Timezone changing(2)

---
+++ auction/src/main/java/com/memepatentoffice/auction/config/JacksonConfig.java
@@ -0,0 +1,16 @@
+package com.memepatentoffice.auction.config;
+
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import org.springframework.boot.autoconfigure.jackson.Jackson2ObjectMapperBuilderCustomizer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class JacksonConfig {
+
+    @Bean
+    public Jackson2ObjectMapperBuilderCustomizer jsonCustomizer() {
+        return builder -> builder.featuresToDisable(DeserializationFeature.ADJUST_DATES_TO_CONTEXT_TIME_ZONE);
+    }
+
+}
