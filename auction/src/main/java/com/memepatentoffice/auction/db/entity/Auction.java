bf41abdf9081e4666298caa505abfb1652070cd1
안려환 <arh951019@gmail.com>
2023-04-09 14:41:42 +0900

    [BE] feat: update auction time to 15

--- auction/src/main/java/com/memepatentoffice/auction/db/entity/Auction.java
+++ auction/src/main/java/com/memepatentoffice/auction/db/entity/Auction.java
@@ -36,7 +36,7 @@ public class Auction extends BaseEntity{
     @Enumerated(EnumType.STRING)
     @Column(name = "status")
     private AuctionStatus status;
-
+    // time
     private static final Integer AUCTION_DURATION_MINUTES = 15;
     @Builder
     public Auction(Long memeId, LocalDateTime startTime, Long sellerId,
